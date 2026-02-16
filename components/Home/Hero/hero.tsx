03e19855ed57e26644ea4f027ac9774bbc3f1bb0
shine7on <pabboo13@gmail.com>
2026-02-15 22:26:51 -0600

    Add text and text-write effect [npm i typewriter-effect]

--- components/Home/Hero/hero.tsx
+++ components/Home/Hero/hero.tsx
@@ -1,11 +1,40 @@
+"use client";
+// top area of the website: hero images, text effects
 import Image from "next/image";
 import React from "react";
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
     return <div className="relative h-screen flex items-center justify-center text-white overflow-hidden flex-col">
         <div className="relative z-10 flex flex-col items-center">
-            <Image src="/images/s1.jpg" alt="heroimage" width={150} height={150} className="rounded-full border-8
-            border-[#0c0c48aa]"/>
+            <Image 
+                src="/images/s1.jpg" 
+                alt="heroimage" 
+                width={150} 
+                height={150} 
+                className="rounded-full border-8 border-[#0c0c48aa]"/>
+            <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 text-center font-bold tracking-wide">
+                Creating web products, <br />
+                brands, 
+                <span className="text-cyan-200"> and experiences.</span>
+            </h1>
+            <h2 className="mt-5 text-sm px-2 sm:text-2xl font-medium flex items-center gap-2">
+                Hi! I'm Shinon - A Passionate
+                <span className="text-cyan-200 font-bold">
+                    <Typewriter options={{
+                        strings:[
+                            " Frontend Developer",
+                            "Backend Developer",
+                            "Web Developer",
+                        ],
+                        autoStart: true,
+                        loop: true,
+                        delay: 75,
+                        deleteSpeed: 50,
+                        wrapperClassName: "p1-2",
+                    }}/>
+                </span>
+            </h2>
         </div>
     </div>;
 };
