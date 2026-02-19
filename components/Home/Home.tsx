f30d7c23b2202689b84678d830aca2355a500afd
shine7on <pabboo13@gmail.com>
2026-02-19 09:58:02 -0600

    Add services page (skill sets page)

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero/hero';
+import Services from './Services/services'
 // heavy thing will be here
 
 const Home = () => {
     return <div className = "overflow-hidden">
         <Hero />
+        <Services />
     </div>;
 };
 
